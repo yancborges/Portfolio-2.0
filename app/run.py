7ad2cb4ff142fc8500b5f075d71c3f606ccc1e9c
Yan <yan.cborges@gmail.com>
2020-03-15 12:34:45 -0300

    adding kaggle section at projects

--- app/run.py
+++ app/run.py
@@ -5,6 +5,16 @@ app = Flask(__name__)
 
 app.register_blueprint(routes)
 
+def connect():
+
+    with open('C://Users//Yan//Desktop//Scripting//doenteMental.py//kaggle scrapper//mongo_str.txt', 'r') as f:
+        mongo_str = f.read()
+        
+    client = MongoClient(mongo_str)
+    db = client.kaggle
+    return db
+
+
 if __name__ == '__main__':
     app.secret_key='hiddenpass'
     app.run(debug=True)
